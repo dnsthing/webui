db2a6237f1dae04df285f8d58b984b977e13e3cb
Aidan Burchett <aidan@burchett.me>
2024-09-14 12:51:45 -0500

    sidebar 1 commit

--- hackwest-website/src/App.jsx
+++ hackwest-website/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import './App.css'
+import './content.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -7,8 +8,21 @@ function App() {
   return (
     <>
       <header>
-        <h1>Chat</h1>
+        <h1>DNS Thing</h1>
       </header>
+
+      <div class = "sidebar">
+        <a href = "#home">Dashboard</a>
+        <a href = "ad_lists">Ad Lists</a>
+        <a href = "domain">Domains</a>
+        <a href = "settings">Settings</a>
+      </div>
+
+      
+
+      <div class = "pictures">
+
+      </div>
     </>
   )
 }
---
+++ hackwest-website/src/content.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function Content() {
+
+
+    return (
+        <>
+        <h1>
+            Content
+        </h1>
+
+        </>
+    )
+}
